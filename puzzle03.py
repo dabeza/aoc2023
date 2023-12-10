4a89d1d15c80dc9be2506e59173026295235e8f8
David Hill <dr.david.hill@pm.me>
2023-12-10 13:15:01 +0100

    day 3 puzzle 2

--- puzzle03.py
+++ puzzle03.py
@@ -1,60 +1,103 @@
 import elf
 import sys
-        
-        
+
+
+def check_perimiter_char(char, star_dict, row, col, number):
+    if char != ".":
+        if char == "*":
+            star_coord = (row, col)
+            add_star_dict(star_dict, star_coord, number)
+        return True
+    return False
+
+
+def add_star_dict(star_dict: dict, star_coord, number):
+    if star_coord in star_dict.keys():
+        star_dict[star_coord].append(int(number))
+    else:
+        star_dict[star_coord] = [int(number)]
+
 
 def puzzler(file_name):
     data = elf.get_data(file_name)
     sum1 = 0
-    file = open("output03.txt","w")
-    for i in range(len(data)):
-        cur=data[i].strip()
-        if i >0:
-            prv=data[i-1].strip()
-        else :
-            prv = "." * len(cur)    
-        
-        if i < len(data)-1:
-            nxt = data[i+1].strip()
+    file = open("output03.txt", "w")
+    columns = len(data[0].strip())
+    star_dict = {}
+    for row in range(len(data)):
+        cur = data[row].strip()
+        if row > 0:
+            prv = data[row - 1].strip()
         else:
-            nxt = "." * len(cur)
-        j = 0
-        while j < len(cur):
-            number =""
+            prv = "." * columns
+
+        if row < len(data) - 1:
+            nxt = data[row + 1].strip()
+        else:
+            nxt = "." * columns
+        col = 0
+        while col < columns:
+            number = ""
             start = None
-            while j<len(cur) and cur[j].isdigit():
+            while col < columns and cur[col].isdigit():
                 if start is None:
-                    start = j -1
+                    start = col - 1
                 if start < 0:
                     start = 0
 
-                number +=cur[j]
-                j += 1
+                number += cur[col]
+                col += 1
             if number:
-                stop = j + 1
-                if stop > len(cur):
-                    stop = j
-                print(number,file=file)
-                perimiter = ""
-                perimiter += prv[start:stop]
-                if start > 0:
-                    perimiter += cur[start]
-                if stop <len(cur):
-                    perimiter += cur[stop-1]
-                perimiter += nxt[start:stop]
-                print(perimiter,file=file)
-                print("",file=file)
-                for char in perimiter:
-                    if char != "." and not char.isdigit():
-                        sum1 += int(number)
-                        break
-            j+=1
-            
-                
+                stop = col + 1
+                if stop > columns:
+                    stop = col
+                is_part_number = False
 
+                # top
+                sub_string = prv[start:stop]
+                for sub_string_column in range(len(sub_string)):
+                    char = sub_string[sub_string_column]
+                    is_part_number |= check_perimiter_char(
+                        char, star_dict, row - 1, sub_string_column + start, number
+                    )
+
+                # left
+                if start > 0:
+                    char = cur[start]
+                    is_part_number |= check_perimiter_char(
+                        char, star_dict, row, start, number
+                    )
+
+                # right
+                if stop < columns:
+                    char = cur[stop - 1]
+                    is_part_number |= check_perimiter_char(
+                        char, star_dict, row, stop - 1, number
+                    )
+
+                # bottom
+                sub_string = nxt[start:stop]
+                for sub_string_column in range(len(sub_string)):
+                    char = sub_string[sub_string_column]
+                    is_part_number |= check_perimiter_char(
+                        char, star_dict, row + 1, sub_string_column + start, number
+                    )
+
+                if is_part_number:
+                    sum1 += int(number)
+            col += 1
+
+    sum2 = 0
+    print(star_dict, file=file)
+    for numbers in list(star_dict.values()):
+        if len(numbers) == 2:
+            # print(numbers)
+            gear_ratio = numbers[0] * numbers[1]
+            sum2 += gear_ratio
 
     print(f"The answer to q1 using {file_name} is : {sum1}")
-            
+    print(f"The answer to q2 using {file_name} is : {sum2}")
+    print("")
 
 
 puzzler("example03-1.txt")
